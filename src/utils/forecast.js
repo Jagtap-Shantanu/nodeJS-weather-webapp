d803cc256c5785c008065e493a0be8a969578213
Shantanu Jagtap <shantanujagtap93@gmail.com>
2021-06-10 16:23:19 +0530

    Humidity data added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
                 callback('Unable to find. Try another search.')
             }
             else {
-                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%')
             }
         })
 
